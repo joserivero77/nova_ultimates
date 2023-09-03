6ae920394baf39847fb9d6f240b5a67a4e452ebb
José Rafael Gutierrez <josefo727@gmail.com>
2023-09-02 23:01:06 -0500

    Feat: demo color change

--- resources/views/layouts/amd.blade.php
+++ resources/views/layouts/amd.blade.php
@@ -68,7 +68,7 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
-                    <span>Gestion de Productos</span>
+                    <span style="color: red">Gestion de Productos</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
